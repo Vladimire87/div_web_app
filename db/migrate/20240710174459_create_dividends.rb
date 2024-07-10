c57bcfec8af63d00e175de8213822ca7ef843b0f
Vladimir Egorov <wahaplay@gmail.com>
2024-07-10 20:45:45 +0300

    add Dividend model

---
+++ db/migrate/20240710174459_create_dividends.rb
@@ -0,0 +1,13 @@
+class CreateDividends < ActiveRecord::Migration[7.1]
+  def change
+    create_table :dividends do |t|
+      t.references :stock, null: false, foreign_key: true
+      t.date :ex_dividend_date
+      t.date :payment_date
+      t.float :amount
+      t.string :frequency
+
+      t.timestamps
+    end
+  end
+end
