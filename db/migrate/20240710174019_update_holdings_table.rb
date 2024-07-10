00943506c43f4a81eb98bc01356a8ca4f9fee021
Vladimir Egorov <wahaplay@gmail.com>
2024-07-10 20:41:55 +0300

    holdings model update

---
+++ db/migrate/20240710174019_update_holdings_table.rb
@@ -0,0 +1,7 @@
+class UpdateHoldingsTable < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :holdings, :stock, foreign_key: true
+    add_column :holdings, :average_buy_price, :float
+    remove_column :holdings, :ticker, :string
+  end
+end
